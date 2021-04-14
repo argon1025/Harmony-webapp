fe2bf10900c478931365cd8359ae68bb858b2326
argon1025 <argon1025@gmail.com>
2021-04-14 11:15:49 +0900

    Add ReduxThunk 추가 및 테스트 진행

---
+++ src/action/RESTAPI.js
@@ -0,0 +1,3 @@
+export const getComments = (testValue) => async (dispatch, getState) => {
+    dispatch({ type: "ServiceInformaion/SET_TEXT", size: "pong" });
+}
--- src/containers/ServiceInformaion.js
+++ src/containers/ServiceInformaion.js
@@ -1,18 +1,19 @@
 import { ServiceInformaionComponent } from "../components";
 import { connect } from "react-redux";
+import * as RESTAPI from "../action/RESTAPI";
 
 function reduxStateToReactProps(state) {
   // 리덕스 스토어 상태를 전달
   return {
     APP_VERSION: state.APP_VERSION,
     TEST_VALUE: state.TEST_VALUE,
-  }; 
+  };
 }
 function reduxDispatchToReactProps(dispatch) {
   // dispatch 메서드를 전달
   return {
     ping: (text) => {
-      dispatch({ type: "ServiceInformaion/SET_TEXT", size: text });
+      dispatch(RESTAPI.getComments(text));
     },
   };
 }
