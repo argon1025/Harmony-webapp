9dd28bfd203ecd3e8ade698bd3c6c30a910b6d99
argon1025 <argon1025@gmail.com>
2021-04-15 11:08:19 +0900

    소셜로그인 테스트를 위한 임시 릴리즈

--- README.md
+++
@@ -1,8 +0,0 @@
-# Harmony-webApp
-> 🔧 Project in development 🔧 
-
-주니어 개발자를 위한 팀 구인 플랫폼
-
-Team recruitment platform for junior developers
-
-Backend-server repositories is [HERE](https://github.com/argon1025/Harmony-serverless-backend/edit/main/README.md)
--- src/containers/index.js
+++
@@ -1,6 +0,0 @@
-import ServiceInformaion from "./ServiceInformaion";
-import NavigationBar from "./NavigationBar";
-import MainIntroduction from "./MainIntroduction";
-import FooterBar from "./FooterBar";
-import KakaoLogin from "./KakaoLogin";
-export { ServiceInformaion, NavigationBar, MainIntroduction, FooterBar, KakaoLogin};
