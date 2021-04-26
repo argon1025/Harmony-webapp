a9289c32a995926036b575ffadf29087b38bee1a
argon1025 <argon1025@gmail.com>
2021-04-26 16:23:14 +0900

    Add 로그인 구현, 미 회원가입시 리 다이렉트 close #11

--- src/action/RESTAPI.js
+++ src/action/RESTAPI.js
@@ -1,37 +1,96 @@
 /**
  * Action
- * 
+ *
  * Redux 에서 상태에 변화가 필요할때 발생되는 액션
  */
 import Axios from "axios";
 import * as modal from "../action/modal";
 
 export const setTestValue = (testValue) => async (dispatch, getState) => {
-    dispatch({ type: "ServiceInformaion/SET_TEXT", size: "pong" });
-}
+  dispatch({ type: "ServiceInformaion/SET_TEXT", size: "pong" });
+};
 
 //인가코드를 이용해서 카카오 토큰정보를 로드
 export const getKakaoToken = (kakaoCode) => async (dispatch, getState) => {
-    const KAKAO_URL_HOST = "https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=dab58a800514cc429576cf0f84c83ab0&redirect_uri=http://harmony.seongrok.net/kakao/auth&code=";
-    const HARMONY_URL_HOST = "https://r5fg3qi652.execute-api.ap-northeast-2.amazonaws.com/dev1/api/v1/kakao/authorize"
-    try {
-        //카카오 서버로부터 토큰 로드
-        const result = await Axios.get(`${KAKAO_URL_HOST}${kakaoCode}`);
-        console.log(result);
+  const KAKAO_URL_HOST =
+    "https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=dab58a800514cc429576cf0f84c83ab0&redirect_uri=http://harmony.seongrok.net/kakao/auth&code=";
+  const HARMONY_URL_HOST =
+    "https://r5fg3qi652.execute-api.ap-northeast-2.amazonaws.com/dev1/api/v1/kakao/authorize";
+  try {
+    //카카오 서버로부터 토큰 로드
+    const result = await Axios.get(`${KAKAO_URL_HOST}${kakaoCode}`);
 
-        // 토큰정보 스토어 적재
-        const accessToken = result.data.access_token;
-        const refreshToken = result.data.refresh_token;
-        dispatch({type:"Service/SET_TOKEN_INFO",size:{accessToken:accessToken,refreshToken:refreshToken}});
+    // 사용자 정보 콘솔화면 표시
+    //console.log(result);
 
-    } catch (error) {
-        //console.log(error.response);
-        dispatch(modal.openErrorMessage("카카오톡 아이디 정보를 로드하는데 실패했습니다.","/"));
+    // 토큰정보 스토어 적재
+    const accessToken = result.data.access_token;
+    const refreshToken = result.data.refresh_token;
+    dispatch({
+      type: "Service/SET_TOKEN_INFO",
+      size: { accessToken: accessToken, refreshToken: refreshToken },
+    });
+  } catch (error) {
+    //콘솔 에러 출력
+    //console.log(error.response);
+    dispatch(
+      modal.openErrorMessage(
+        "카카오톡 아이디 정보를 로드하는데 실패했습니다 잠시후 다시 시도해 주세요.",
+        "/"
+      )
+    );
+  }
+  try {
+    // 하모니 서버에 가입유무 확인
+    //console.log(`${getState().ACCESS_TOKEN} --------> 가입확인`)
+    console.log(`${HARMONY_URL_HOST}`);
+    const result = await Axios.get(`${HARMONY_URL_HOST}`, {
+      headers: {
+        userToken: getState().ACCESS_TOKEN,
+        userTokenType: "kakao",
+      },
+    });
+    //회원 데이터 출력
+    //console.log(result);
+    dispatch({
+      type: "Service/SET_USER_INFO",
+      size: {
+        USER_ID: result.data.data.id,
+        USER_JOBTAG: result.data.data.jobTag,
+        USER_NAME: result.data.data.name,
+        USER_PROFILE: result.data.data.profileImageUrl,
+      },
+    });
+
+    //로그인 성공후에 메인 페이지로 이동
+    window.location.pathname = "/";
+  } catch (error) {
+    //콘솔 에러 출력
+    //console.log(error);
+    if (error.response) {
+      // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
+      //console.log(error.response.data); // 서버응답
+      //console.log(error.response.status); //400
+      //console.log(error.response.headers);
+      dispatch(modal.openErrorMessage("회원등록이 필요합니다", "/signup"));
+    } else if (error.request) {
+      // 요청이 이루어 졌으나 응답을 받지 못했습니다.
+      // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+      // Node.js의 http.ClientRequest 인스턴스입니다.
+      //console.log(error.request);
+      dispatch(
+        modal.openErrorMessage(
+          "서비스가 오프라인 상태 입니다 잠시후 다시 시도해 주세요.",
+          "/"
+        )
+      );
+    } else {
+      dispatch(
+        modal.openErrorMessage(
+          "카카오톡 아이디 정보를 로드하는데 실패했습니다 잠시후 다시 시도해 주세요.",
+          "/"
+        )
+      );
     }
-    try {
-        console.log(`${getState().ACCESS_TOKEN} --------> 가입확인`)
-        //const harmonyResult = await Axios.get(`${HARMONY_URL_HOST}${accessToken}`);
-    } catch (error) {
-        dispatch(modal.openErrorMessage("카카오톡 아이디 정보를 로드하는데 실패했습니다.","/"));
-    }
-}
+  }
+};
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,8 +2,8 @@ const initState = {
   APP_VERSION: "0.1",
   TEST_VALUE: 0,
 
-  ACCESS_TOKEN:"",
-  REFRESH_TOKEN:"",
+  ACCESS_TOKEN: "",
+  REFRESH_TOKEN: "",
 
   USER_LOGIN: false,
   USER_ID: "",
@@ -12,9 +12,9 @@ const initState = {
   USER_PROFILE: "",
   USER_LOGIN_TYPE: "",
 
-  ERROR_STATE: true,
+  ERROR_STATE: false,
   ERROR_MSG: "",
-  ERROR_TARGET_PATH:"",
+  ERROR_TARGET_PATH: "",
 };
 
 const reducer = (state = initState, action) => {
@@ -25,11 +25,30 @@ const reducer = (state = initState, action) => {
     case "ServiceInformaion/SET_TEXT":
       return { ...state, TEST_VALUE: action.size };
     case "Service/ERROR_MODAL_OPEN":
-      return { ...state, ERROR_MSG: action.size.msg, ERROR_STATE: true,ERROR_TARGET_PATH: action.size.path};
+      return {
+        ...state,
+        ERROR_MSG: action.size.msg,
+        ERROR_STATE: true,
+        ERROR_TARGET_PATH: action.size.path,
+      };
     case "Service/ERROR_MODAL_CLOSE":
       return { ...state, ERROR_MSG: "", ERROR_STATE: false };
-      case "Service/SET_TOKEN_INFO":
-        return { ...state, ACCESS_TOKEN: action.size.accessToken, REFRESH_TOKEN: action.size.refreshToken };
+    case "Service/SET_TOKEN_INFO":
+      return {
+        ...state,
+        ACCESS_TOKEN: action.size.accessToken,
+        REFRESH_TOKEN: action.size.refreshToken,
+      };
+    case "Service/SET_USER_INFO":
+      return {
+        ...state,
+        USER_LOGIN: true,
+        USER_ID: action.size.USER_ID,
+        USER_JOBTAG: action.size.USER_JOBTAG,
+        USER_NAME: action.size.USER_NAME,
+        USER_PROFILE: action.size.USER_PROFILE,
+        USER_LOGIN_TYPE: "kakao",
+      };
     default:
       return state;
   }
