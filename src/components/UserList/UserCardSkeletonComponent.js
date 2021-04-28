abc53cd5c26e924587e68e0212179e1b2979defa
argon1025 <argon1025@gmail.com>
2021-04-28 21:06:58 +0900

    Modify UserCard, Skeleton 컴포넌트 테스트 #23

--- src/components/UserList/UserCardSkeletonComponent.js
+++ src/components/UserList/UserCardSkeletonComponent.js
@@ -1,35 +1,20 @@
 import React, { Component } from "react";
 
-export default class UserCard extends Component {
-  state = {
-    name:"",
-    blogLink:"",
-    jobTag:"",
-    profileImage:""
-  }
+export default class UserCardSkeleton extends Component {
   constructor(props) {
     super(props);
-
-    // 상태 설정
-    this.state.name = this.props.name || "User";
-    this.state.blogLink = this.props.blogLink || "http://harmony.seongrok.net";
-    this.state.jobTag = this.props.jobTag || "1";
-    this.state.profileImage = this.props.profileImage || "http://k.kakaocdn.net/dn/5b4p3/btqXfJNCwln/xkWdK3xaopQHiqj3kG3DZ0/img_640x640.jpg";
-
   }
 
   render() {
     return (
-      <div className="w-full lg:w-72 h-56">
+      <div className="w-full lg:w-72 h-56 animate-pulse">
         <div class="relative bg-white rounded-3xl py-6 px-6 my-4 shadow-xl">
-          <div class=" text-white flex items-center absolute rounded-full shadow-xl bg-gray-200 h-16 w-16 left-4 -top-6 overflow-hidden">
-            <img
-              src={this.state.profileImage}
-              alt="UserImage"
-            ></img>
+            {/* Profile Img */}
+          <div class=" text-white flex items-center absolute rounded-full shadow-xl left-4 -top-6 overflow-hidden h-16 w-16 bg-gray-200">
           </div>
           <div class="mt-8">
-            <p class="text-xl font-semibold my-2">{this.state.name}</p>
+              {/* User Name */}
+          <div class="h-6 bg-gray-200 rounded w-3/4"></div>
             <div class="flex space-x-2 text-gray-400 text-sm my-3">
             <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -43,18 +28,15 @@ export default class UserCard extends Component {
                     clip-rule="evenodd"
                   />
                 </svg>
-              <p className="truncate">{this.state.blogLink}</p>
+                {/* Blog Link */}
+                <div class="h-4 bg-gray-200 rounded w-3/4"></div>
             </div>
             <div class="border-t-2"></div>
 
             <div class="flex justify-between">
               <div class="my-2">
-                <p class="font-semibold text-base mb-2">전문분야</p>
-                <td class="text-center">
-                <span class="bg-green-200 text-green-600 py-1 px-3 rounded-full text-xs">
-                  BackEnd
-                </span>
-              </td>
+                <div class="h-6 bg-gray-200 rounded w-14 my-2"></div>
+                <div class="h-4 bg-gray-200 rounded w-3/4"></div>
               </div>
             </div>
           </div>
--- src/components/UserList/UserListComponent.js
+++ src/components/UserList/UserListComponent.js
@@ -1,42 +1,61 @@
 import React, { Component } from "react";
 import UserCard from "./UserCardComponent";
+import UserCardSkeleton from "./UserCardSkeletonComponent";
+
 export default class UserList extends Component {
+  state = {
+    pageLoading: true,
+  };
   constructor(props) {
     super(props);
+  }
+  async componentDidMount() {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 5000);
+    });
+    this.setState({ pageLoading: false });
   }
 
   render() {
     return (
       <div className="min-h-screen bg-gray-50 justify-center">
         <div className="flex flex-row justify-center lg:py-28">
-          <p className="text-xl text-gray-700 lg:text-4xl pt-5">프로젝트를 시작하기전 어떤 사람들이 있는지 확인하세요!</p>
+          <p className="text-xl text-gray-700 lg:text-4xl pt-5">
+            프로젝트를 시작하기전 어떤 사람들이 있는지 확인하세요!
+          </p>
         </div>
-        <div className="flex flex-col lg:flex-row flex-wrap justify-center mr-10 ml-10 lg:mr-28 lg:ml-28">
-          <div className="m-5">
-            <UserCard name="test" blogLink="test.com" jobTag="1" />
+        {/* 페이지 로딩 */}
+        {this.state.pageLoading ? (
+          <div className="flex flex-col lg:flex-row flex-wrap justify-center mr-10 ml-10 lg:mr-28 lg:ml-28">
+            <div className="m-5">
+              <UserCardSkeleton />
+            </div>
+            <div className="m-5">
+              <UserCardSkeleton />
+            </div>
+            <div className="m-5">
+              <UserCardSkeleton />
+            </div>
+            <div className="m-5">
+              <UserCardSkeleton />
+            </div>
           </div>
-          <div className="m-5">
-            <UserCard />
+        ) : (
+          <div className="flex flex-col lg:flex-row flex-wrap justify-center mr-10 ml-10 lg:mr-28 lg:ml-28">
+            <div className="m-5">
+              <UserCard name="test" blogLink="test.com" jobTag="1" />
+            </div>
+            <div className="m-5">
+              <UserCard name="test" blogLink="test.com" jobTag="1" />
+            </div>
+            <div className="m-5">
+              <UserCard name="test" blogLink="test.com" jobTag="1" />
+            </div>
+            <div className="m-5">
+              <UserCard name="test" blogLink="test.com" jobTag="1" />
+            </div>
           </div>
-          <div className="m-5">
-            <UserCard />
-          </div>
-          <div className="m-5">
-            <UserCard />
-          </div>
-          <div className="m-5">
-            <UserCard />
-          </div>
-          <div className="m-5">
-            <UserCard />
-          </div>
-          <div className="m-5">
-            <UserCard />
-          </div>
-          <div className="m-5">
-            <UserCard />
-          </div>
-        </div>
+        )}
       </div>
     );
   }
